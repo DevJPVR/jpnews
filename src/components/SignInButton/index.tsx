cf9b26003dfbd6e936607b2a9fda36ecd83d0c0e
Joao Pedro <dev.joaopedrovieira@gmail.com>
2021-11-19 21:08:34 +0000

    first commit

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,35 @@
+import { signIn, signOut, useSession } from 'next-auth/client'
+
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+
+
+import styles from './styles.module.scss'
+
+export function SignInButton() {
+    const [session] = useSession();
+
+    console.log(session);
+
+    return session ? (
+    <button 
+        type="button"
+        className={styles.SignInButton}
+        onClick={() => signOut()}
+    >
+    <FaGithub color="#04d361" />
+       {session.user.name}
+    <FiX color="#737390" className={styles.closeIcon} />
+    </button>
+   
+    ) : (
+     <button
+         type="button"
+         className={styles.SignInButton}
+         onClick={() => signIn('github')}
+     >
+     <FaGithub color="#eba417" />
+        Sign in with Github
+     </button>
+    );
+}
