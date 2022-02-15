6f4e954d4dfd796d1f60609d50fdc1661537bd29
Joao Pedro Vieira <dev.joaopedrovieira@gmail.com>
2022-02-15 12:53:01 +0000

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
+import  Image  from 'next/image'
 
 import styles from './home.module.scss';
 
@@ -31,7 +32,7 @@ export default function Home({ product }: HomeProps) {
             <SubscribeButton priceId={product.priceId}/>
           </section>
 
-          <img src="/images/avatar.svg" alt="Girl coding" />
+          <Image src="/images/avatar.svg" width="336" height="521" alt="Girl coding" />
         </main>
     </>
   )
